594cf8ad67469d35b74797f66c6adf131c7ed57c
Patrick Williams <pwmckenna@gmail.com>
2019-01-16 17:56:20 -0800

    ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,12 @@ module.exports = {
         icon: `src/images/logo.jpeg`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-132688472-1'
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
