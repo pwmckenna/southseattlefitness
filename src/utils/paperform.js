e037746398e1bb8ed0ebf2e53d3e5744efccfc3d
Patrick Williams <patrickw@porch.com>
2019-03-22 17:04:47 -0700

    paperform utils

---
+++ src/utils/paperform.js
@@ -0,0 +1,15 @@
+import { useEffect } from 'react';
+import { createScript } from '.';
+
+export const load = () => createScript('https://paperform.co/__embed');
+export const show = () => window.Paperform.popup('you1pfhj');
+export const use = () => {
+    useEffect(() => {
+        load();
+        if (!localStorage.getItem('paperform')) {
+            localStorage.setItem('paperform', true);
+            setTimeout(show, 1000);
+        }
+    }, []);
+};
+
